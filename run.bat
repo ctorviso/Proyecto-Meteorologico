48714ac2ee3cfd29a901f055ef63ce1e33bd2157
Sara García <saragarcia6123@gmail.com>
2025-02-24 14:16:50 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,12 +8,12 @@ if not exist logs (
 
 run_tests.bat
 
-echo "Starting FastAPI..."
+echo Starting FastAPI...
 
 :: Start FastAPI
 start "" uvicorn src.api.main:app --reload --host localhost --port 8000 >> logs\fastapi.log 2>&1
 
-echo "Starting Streamlit..."
+echo Starting Streamlit...
 
 :: Start Streamlit
 start "" streamlit run streamlit_app.py --server.port 8501 --server.address localhost >> logs\streamlit.log 2>&1

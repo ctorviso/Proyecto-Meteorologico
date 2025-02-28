d6b2aef6a2f914cceda204055b9aaa712a42ceee
saragarcia6123 <saragarcia6123@gmail.com>
2025-02-28 20:22:35 +0100

    Split historical into years

--- etl_scripts/pipeline.py
+++ etl_scripts/pipeline.py
@@ -43,9 +43,14 @@ def filter_date_range(start_date, end_date):
     return new_start_date, new_end_date
 
 async def run_etl(start_date: date, end_date: date):
+
+    if start_date.year != end_date.year:
+        logger.error("Start and end date must be in the same year. To process multiple years, run ETL for each year separately.")
+        return
+
     logger.info(f"Running ETL for date range {start_date} to {end_date}...")
 
-    #start_date, end_date = filter_date_range(start_date, end_date)
+    start_date, end_date = filter_date_range(start_date, end_date)
     if start_date is None:
         return
 
@@ -74,8 +79,10 @@ async def run_etl(start_date: date, end_date: date):
     elapsed_time = time.time() - start_time
     logger.info(f"Total execution time for all tables: {elapsed_time:.2f} seconds")
 
-    df.to_csv(os.path.join(script_dir, '../data/historical/historico.csv'), mode='a', header=False, index=False)
-    avg_df.to_csv(os.path.join(script_dir, '../data/historical/historico_avg.csv'), mode='a', header=False, index=False)
+    year = start_date.year
+
+    df.to_csv(os.path.join(script_dir, f'../data/historical/historico/{year}.csv'), mode='a', header=False, index=False)
+    avg_df.to_csv(os.path.join(script_dir, f'../data/historical/historico_avg/{year}.csv'), mode='a', header=False, index=False)
 
     logger.info("ETL process completed.")
 

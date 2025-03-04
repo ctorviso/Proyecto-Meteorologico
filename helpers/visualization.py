4f974b25535bafee762c5d1b28624ea6d035aa86
saragarcia6123 <saragarcia6123@gmail.com>
2025-03-04 03:09:50 +0100

    Provincia average bar plots

--- helpers/visualization.py
+++ helpers/visualization.py
@@ -3,7 +3,6 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from tensorflow.python.data.ops.optional_ops import Optional
 
 
 def histogram(

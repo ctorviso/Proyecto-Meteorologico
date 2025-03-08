f702077872fe22029cca92475262e876d2ee9637
saragarcia6123 <saragarcia6123@gmail.com>
2025-03-08 11:39:58 +0100

    Update datasets page

--- src/streamlit_app/Pages/Datasets/Datasets-ML.py
+++ src/streamlit_app/Pages/Datasets/Datasets-ML.py
@@ -8,12 +8,16 @@ BASE_URL = "https://rednvrsdcuwtwyfxtjru.supabase.co/storage/v1/object/public/ml
 
 files = [
     {"name": "train.csv", "description": "Training dataset", "url": f"{BASE_URL}/train.csv"},
-    {"name": "test.csv", "description": "Testing dataset", "url": f"{BASE_URL}/test.csv"},
     {"name": "validation.csv", "description": "Validation dataset", "url": f"{BASE_URL}/validation.csv"},
+    {"name": "test.csv", "description": "Testing dataset", "url": f"{BASE_URL}/test.csv"},
     {"name": "train_scaled.csv", "description": "Scaled training dataset", "url": f"{BASE_URL}/train_scaled.csv"},
+    {"name": "validation_scaled.csv", "description": "Scaled validation dataset", "url": f"{BASE_URL}/validation_scaled.csv"},
     {"name": "test_scaled.csv", "description": "Scaled testing dataset", "url": f"{BASE_URL}/test_scaled.csv"},
-    {"name": "validation_scaled.csv", "description": "Scaled validation dataset",
-     "url": f"{BASE_URL}/validation_scaled.csv"},
+
+    {"name": "scaler_X_full.joblib", "description": "Scaler for X_full", "url": f"{BASE_URL}/scaler_X_full.joblib"},
+    {"name": "scaler_y_full.joblib", "description": "Scaler for y_full", "url": f"{BASE_URL}/scaler_y_full.joblib"},
+    {"name": "scaler_X_train.joblib", "description": "Scaler for X_train", "url": f"{BASE_URL}/scaler_X_train.joblib"},
+    {"name": "scaler_y_train.joblib", "description": "Scaler for y_train", "url": f"{BASE_URL}/scaler_y_train.joblib"},
 ]
 
 st.title("📊 Datasets para los modelos de Machine Learning")
